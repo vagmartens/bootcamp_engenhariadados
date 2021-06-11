e3ae6be99fa7fd2c82bc963120fb2d9ccfe72075
vagmartens <83595636+vagmartens@users.noreply.github.com>
2021-06-10 21:48:43 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,5 +1,9 @@
-import fake-web-events
+from fake_web_events import Simulation
+import json
+
+simulation = Simulation(user_pool_size=100, sessions_per_day=100000)
+events = simulation.run(duration_seconds=60)
 
 for event in events:
-    print(events)
+    print(json.load(event))
     break
